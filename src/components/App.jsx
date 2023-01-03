1bd2e88844afc6a3ad34e25e5b9bc354e830b3e7
MaksymilianBilski <biger9@o2.pl>
2023-01-03 06:00:25 +0100

    first task functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,15 @@
+import Profile from '../components/Profile/Profile.jsx';
+import user from '../components/data/user.json';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Profile
+        avatar={user.avatar}
+        tag={user.tag}
+        loccation={user.loaction}
+        stats={user.stats}
+      />
+    </div>
+  );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import css from '../Profile/Profile.module.css';
+
+const Profile = ({ avatar, username, tag, location, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  stats: PropTypes.objectOf({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+};
+
+export default Profile;
