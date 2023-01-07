67f0cd265c2cda7d28e04dd69baa3d72103a7ac4
MaksymilianBilski <biger9@o2.pl>
2023-01-07 13:57:15 +0100

    reset

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,3 @@
-import Profile from '../components/Profile/Profile.jsx';
-import { Statistics } from './Statistics/Statistics.jsx';
-import { FriendList } from './FriendList/FriendList.jsx';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
-import user from '../components/data/user.json';
-import data from '../components/data/data.json';
-import friends from '../components/data/friends.json';
-import transactions from '../components/data/transactions.json';
-
 export const App = () => {
-  return (
-    <div>
-      <Profile
-        avatar={user.avatar}
-        tag={user.tag}
-        loccation={user.loaction}
-        stats={user.stats}
-      />
-      <Statistics title="upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
-  );
+  return <div></div>;
 };
