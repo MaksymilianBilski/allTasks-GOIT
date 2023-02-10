35065d3b5b1976e1cf5255007e59119ee3bebeb5
Maksymilian Bilski <biger9@o2.pl>
2023-02-10 18:40:36 +0100

    navigation styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,24 @@
 import { NavLink } from 'react-router-dom';
+import css from './Navigation.module.css';
 
 const Navigation = () => {
   return (
-    <div>
-      <NavLink to="/react-01-goit/movies/moviesList">Movie finder </NavLink>
-      <NavLink to="/react-01-goit/phonebook">Phonebook </NavLink>
-      <NavLink to="/react-01-goit/feedback">Feedback </NavLink>
-      <NavLink to="/react-01-goit/images">Images finder </NavLink>
-      <NavLink to="/react-01-goit/tasksManager">Task manager </NavLink>
+    <div className={css.navigationWrapper}>
+      <NavLink className={css.navLink} to="/react-01-goit/movies/moviesList">
+        Movie finder
+      </NavLink>
+      <NavLink className={css.navLink} to="/react-01-goit/phonebook">
+        Phonebook
+      </NavLink>
+      <NavLink className={css.navLink} to="/react-01-goit/feedback">
+        Feedback
+      </NavLink>
+      <NavLink className={css.navLink} to="/react-01-goit/images">
+        Images finder
+      </NavLink>
+      <NavLink className={css.navLink} to="/react-01-goit/tasksManager">
+        Task manager
+      </NavLink>
     </div>
   );
 };
