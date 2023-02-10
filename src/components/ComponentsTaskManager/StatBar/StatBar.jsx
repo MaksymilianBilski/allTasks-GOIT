3765be18dc21753fecac33270f41c904d8d5b8d8
Maksymilian Bilski <biger9@o2.pl>
2023-02-10 18:57:16 +0100

    tasks counter

--- src/components/ComponentsTaskManager/StatBar/StatBar.jsx
+++ src/components/ComponentsTaskManager/StatBar/StatBar.jsx
@@ -1,10 +1,17 @@
-const StatBar = ({ completed, incompleted }) => {
+import { getTasks } from '../redux/tasksSelectors';
+import { useSelector } from 'react-redux';
+
+const StatBar = () => {
+  const tasks = useSelector(getTasks);
+  const completedTasks = tasks.filter(el => el.completed).length;
+  const incompletedTasks = tasks.filter(el => !el.completed).length;
+
   return (
     <section>
       <h3>Tasks</h3>
       <ul>
-        <li>completed: {completed}</li>
-        <li>incompleted: {incompleted}</li>
+        <li>completed: {completedTasks}</li>
+        <li>incompleted: {incompletedTasks}</li>
       </ul>
     </section>
   );
