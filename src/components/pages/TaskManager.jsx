49e3afd8154d7a73560e9f1b570253f9a64ffdbe
Maksymilian Bilski <biger9@o2.pl>
2023-02-10 18:49:58 +0100

    redux persist task manager

--- src/components/pages/TaskManager.jsx
+++ src/components/pages/TaskManager.jsx
@@ -1,13 +1,17 @@
 import AppHeader from '../ComponentsTaskManager/AppHeader/AppHeader';
 import TasksList from '../ComponentsTaskManager/TasksList/TasksList';
 import TaskForm from '../ComponentsTaskManager/TaskForm/TaskForm';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from 'store';
 
 export const TaskManager = () => {
   return (
     <>
-      <AppHeader />
-      <TaskForm />
-      <TasksList />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppHeader />
+        <TaskForm />
+        <TasksList />
+      </PersistGate>
     </>
   );
 };
--- src/store.js
+++ src/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { tasksReducer } from 'components/ComponentsTaskManager/redux/tasksReducers';
 import { filterReducer } from 'components/ComponentsTaskManager/redux/filterSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    filters: filterReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  filters: filterReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({ reducer: persistedReducer });
+const persistor = persistStore(store);
+export { store, persistor };
