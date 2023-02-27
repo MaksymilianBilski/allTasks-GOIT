78ec17450671e7fd1100132cf02b0325c34c4845
Maksymilian Bilski <biger9@o2.pl>
2023-02-27 20:07:13 +0100

    Update tasksSelectors.jsx

--- src/components/ComponentsTaskManager/redux/tasksSelectors.jsx
+++ src/components/ComponentsTaskManager/redux/tasksSelectors.jsx
@@ -2,6 +2,9 @@ export const getTasks = state => state.tasks.items;
 
 export const getFilters = state => state.filters.status;
 
-export const getIsLoading = state => state.tasks.isLoading;
+export const getIsLoading = state => {
+  console.log(state);
+  return state.tasks.isLoading;
+};
 
 export const getError = state => state.tasks.error;
