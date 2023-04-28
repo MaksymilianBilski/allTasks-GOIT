df31b63cfde1af053de8460f5c940bb4d6908686
Maksymilian Bilski <biger9@o2.pl>
2023-04-28 16:01:59 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,8 +25,8 @@ export class App extends Component {
         <Route path="/allTasks-GOIT/tasksManager" element={<TaskManager />} />
         <Route path="/allTasks-GOIT/movies" element={<MoviesFinder />}>
           <Route path="moviesList" element={<TrendingList />} />
-          <Route path="searchbar" element={<Searchbar />} />
         </Route>
+        <Route path="searchbar" element={<Searchbar />} />
         <Route path="details/:movieId" element={<MovieDetails />}>
           <Route path="credits" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
